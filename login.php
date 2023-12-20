d0d637fdcc55cf39c275306ced0f35d84fe8d728
Suborno22 <dsuborno0@gmail.com>
2023-12-20 19:19:34 +0530

    Made Changes

--- login.php
+++ login.php
@@ -1,32 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="14">
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <link rel="stylesheet" href="form.css">
-    <title>Login Page</title>
-</head>
-<body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-        <h1>Login</h1>
-        
-        <span>Username</span><input type="text" id="username" name="username" placeholder="Username">
-        <br>
-        <span>Password</span><input type="password" name="password" id="password" placeholder="Password">
-        <br>
-        <input type="submit" name="submit" id="submit" value="Log in">
-    </form>
-    <span>Dont have an account? Click to</span><button onclick="GoToPage()">register</button>
-    <script>
-            function GoToPage(){
-            var a = 'index.php';
-            window.location.href = a;
-    }
-    </script>
-</body>
-</html>
 
 <?php
 
@@ -52,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
                 echo '$(document).ready(function() {';
                 echo '  alert("Successfully Logged in");';
                 echo '  setTimeout(function() {';
-                echo '    window.location.href = "index.php";';
+                echo '    window.location.href = "index.html";';
                 echo '  }, 1000);';
                 echo '});';
                 echo '</script>';
